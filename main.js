1a838ec9b4d2987ebd6f4c6e485baa5574bcbfba
Bernardo Di Leo <bernidileo@gmail.com>
2022-08-17 17:53:23 -0300

    Api key

--- main.js
+++ main.js
@@ -1,18 +1,31 @@
-const API_URL = 'https://api.thecatapi.com/v1/images/search?limit=3';
+const API_URL_RANDOM = 'https://api.thecatapi.com/v1/images/search?limit=2&api_key=c2038054-20cb-493f-96d6-50f3d474c9ef';
+const API_URL_FAVORITES = 'https://api.thecatapi.com/v1/favourites?limit=2&api_key=c2038054-20cb-493f-96d6-50f3d474c9ef';
 
-async function myCat () {
-    const res = await fetch(API_URL);
+const spanError = document.getElementById('error');
+
+async function loadRandomMichis () {
+    const res = await fetch(API_URL_RANDOM);
     const data = await res.json();
-    const img1 = document.getElementById("randomImg1");
-    const img2 = document.getElementById("randomImg2");
-    const img3 = document.getElementById("randomImg3");
-
-    img1.src = data[0].url;
-    img2.src = data[1].url;
-    img3.src = data[2].url;
+    if(res.status !== 200) {
+        spanError.innerHTML = "Error: " + res.status;
+    } else {
+        const img1 = document.getElementById("randomImg1");
+        const img2 = document.getElementById("randomImg2");
+        img1.src = data[0].url;
+        img2.src = data[1].url;
+    }
+    
 }
 
+async function loadFavoriteMichi () {
+    const res = await fetch(API_URL_FAVORITES);
+    const data = await res.json();
+    if(res.status !== 200) {
+        spanError.innerHTML = "Error: " + res.status + " " + data.message;
+    } 
+}
 const myButton = document.getElementById("randomBtt");
-myButton.onclick = myCat;
+myButton.onclick = loadRandomMichis;
 
-myCat();
+loadRandomMichis();
+loadFavoriteMichi();
