0071c1c736f98702a95c53a7ef885c5620e1f7d0
Bernardo Di Leo <bernidileo@gmail.com>
2022-08-17 09:41:55 -0300

    fix erros js and media.css

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-const URL = 'https://api.thecatapi.com/v1/images/search';
+const API_URL = 'https://api.thecatapi.com/v1/images/search';
 
 async function myCat () {
-    const res = await fetch(URL);
+    const res = await fetch(API_URL);
     const data = await res.json();
     const img = document.getElementById("randomImg");
     img.src = data[0].url;
@@ -10,3 +10,4 @@ async function myCat () {
 const myButton = document.getElementById("randomBtt");
 myButton.onclick = myCat;
 
+myCat();
