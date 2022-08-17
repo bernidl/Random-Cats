3efee223dd1e112922df83615332c802d6466c8a
Bernardo Di Leo <bernidileo@gmail.com>
2022-08-17 10:18:40 -0300

    Query parameters

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
-const API_URL = 'https://api.thecatapi.com/v1/images/search';
+const API_URL = 'https://api.thecatapi.com/v1/images/search?limit=3';
 
 async function myCat () {
     const res = await fetch(API_URL);
     const data = await res.json();
-    const img = document.getElementById("randomImg");
-    img.src = data[0].url;
+    const img1 = document.getElementById("randomImg1");
+    const img2 = document.getElementById("randomImg2");
+    const img3 = document.getElementById("randomImg3");
+
+    img1.src = data[0].url;
+    img2.src = data[1].url;
+    img3.src = data[2].url;
 }
 
 const myButton = document.getElementById("randomBtt");
