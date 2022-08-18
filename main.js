04ba1f3d2f7d797503d06e3a2c84122310d926e6
Bernardo Di Leo <bernidileo@gmail.com>
2022-08-18 06:32:38 -0300

    DELETE METOD

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const API_URL_RANDOM = 'https://api.thecatapi.com/v1/images/search?limit=2&api_key=c2038054-20cb-493f-96d6-50f3d474c9ef';
-const API_URL_FAVORITES = 'https://api.thecatapi.com/v1/favourites?limit=2&api_key=c2038054-20cb-493f-96d6-50f3d474c9ef';
+const API_URL_FAVORITES = 'https://api.thecatapi.com/v1/favourites?api_key=c2038054-20cb-493f-96d6-50f3d474c9ef';
+const API_URL_DELETE = (id) => `https://api.thecatapi.com/v1/favourites/${id}?api_key=c2038054-20cb-493f-96d6-50f3d474c9ef`;
 
 const spanError = document.getElementById('error');
 
@@ -11,8 +12,14 @@ async function loadRandomMichis () {
     } else {
         const img1 = document.getElementById("randomImg1");
         const img2 = document.getElementById("randomImg2");
+        const btn1 = document.getElementById("btn1");
+        const btn2 = document.getElementById("btn2");
+
         img1.src = data[0].url;
         img2.src = data[1].url;
+
+        btn1.onclick = () => saveFavoriteMichi(data[0].id);
+        btn2.onclick = () => saveFavoriteMichi(data[1].id);
     }
     
 }
@@ -22,17 +29,39 @@ async function loadFavoriteMichi () {
     const data = await res.json();
     if(res.status !== 200) {
         spanError.innerHTML = "Error: " + res.status + " " + data.message;
-    } 
+    } else {
+        const section = document.getElementById('container-favoriteMichis');
+        section.innerHTML = "";
+        const h2 = document.createElement('h2');
+        const h2text = document.createTextNode('Favorite Cats');
+        h2.appendChild(h2text);
+        section.appendChild(h2);
+
+        data.forEach(michi => {
+            const article = document.createElement('article');
+            const img = document.createElement('img');
+            const btn = document.createElement('button');
+            const btnText = document.createTextNode('Remove michi from favorites');
+
+            btn.appendChild(btnText);
+            btn.onclick = () => deleteFavoriteMichi(michi.id);
+            img.src = michi.image.url;
+            img.width = 150;
+            article.appendChild(img);
+            article.appendChild(btn);
+            section.appendChild(article);
+        })
+    }
 }
 
-async function saveFavoriteMichis(){
+async function saveFavoriteMichi(id){
     const res = await fetch(API_URL_FAVORITES,{
         method: 'POST',
         headers: {
-            'Conten-Type': 'application/json',
+            'Content-Type': 'application/json',
         },
         body: JSON.stringify({
-            image_id: 'dje'
+            image_id: id
         }),
     });
     
@@ -40,8 +69,26 @@ async function saveFavoriteMichis(){
 
     if(res.status !== 200) {
         spanError.innerHTML = "Error: " + res.status + " " + data.message;
-    } 
+    } else {
+        console.log('Michi saved in favorites');
+        loadFavoriteMichi();
+    }
     
+}
+
+async function deleteFavoriteMichi(id){
+    const res = await fetch(API_URL_DELETE(id),{
+        method: 'DELETE',
+    });
+    
+    const data = await res.json();
+
+    if(res.status !== 200) {
+        spanError.innerHTML = "Error: " + res.status + " " + data.message;
+    } else {
+        console.log('Michi deleted from favorites');
+        loadFavoriteMichi();
+    }
 }
 
 const myButton = document.getElementById("randomBtt");
